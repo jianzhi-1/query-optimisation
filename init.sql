1fc06c68aebccf5e03751ca1b3ce3b2d3fbd0cd9
Jianzhi Wang <jianzhiw00@gmail.com>
2025-03-18 21:28:26 -0700

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,5 @@
+-- createuser username || createuser -P username
+-- createdb <db_name>
+-- psql
+CREATE TABLE roster(id int PRIMARY KEY, firstname VARCHAR(40), lastname VARCHAR(40));
+-- \d (lists all relations)
